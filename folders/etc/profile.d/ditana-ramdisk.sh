ef08085c66fee88aa4f557fd01ee8bcee4a9c233
Stefan Zipproth <s.zipproth@acrion.ch>
2026-04-18 17:45:49 +0200

    improved ramdisk, fixed path conventions, added ripgrep config

--- folders/etc/profile.d/ditana-ramdisk.sh
+++ folders/etc/profile.d/ditana-ramdisk.sh
@@ -1,2 +1,4 @@
-# The directory is created via /usr/share/ditana/ramdisk.sh
-export RAMDISK="/run/user/$(id -u)/ramdisk"
+# Expose the RAM-backed scratch directory to interactive login shells
+# (TTY, SSH). The directory itself is created by systemd-tmpfiles (see
+# /usr/share/user-tmpfiles.d/ditana-ramdisk.conf).
+export RAMDISK="$XDG_RUNTIME_DIR/ramdisk"
